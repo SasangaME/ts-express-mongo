e3017c763a4a2ce304e62f13b607a148ee71e297
SasangaME <madhumaledirisinghe@gmail.com>
2022-10-17 11:53:58 +0530

    project setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,46 @@
-console.log("app works")
+import express, { Application, Request, Response, NextFunction } from 'express';
+import { config } from 'dotenv';
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+import { connect, connection } from 'mongoose';
+
+config();
+
+const app: Application = express();
+
+/* Mongo DB */
+const mongoString: string = process.env.MONGO_DB_URL || 'point to localhost or default url';
+connect(mongoString);
+const database = connection;
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+/* */
+
+app.use(cors({ origin: '*' }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Error handler middleware */
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ 'message': err.message });
+    return;
+});
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({ "message": "Express server with TypeScript" });
+});
+
+/* Routes */
+/** */
+
+const port = parseInt(process.env.PORT || '3000');
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+})
